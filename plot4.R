dcefa8d5d7fb31a406229b6a560c2883971b9370
David Pierce <david.pierce@appinions.com>
2015-09-09 21:12:02 -0400

    fixed plot4

--- plot4.R
+++ plot4.R
@@ -15,13 +15,18 @@ if (!exists("power")) {
 
 png("plot4.png")
 p <- par(mfrow=c(2, 2))
+
 plot(power$Time, power$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+
 plot(power$Time, power$Voltage, type="l", col="black", xlab="datetime", ylab="Voltage")
-plot(power$Time, power$Sub_metering_1, type="l", col="black", ylab="Energy sub metering")
+
+plot(power$Time, power$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
 lines(power$Time, power$Sub_metering_2, col="red")
 lines(power$Time, power$Sub_metering_3, col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        bty="n", lty=1, col=c("black", "red", "blue"))
+
 plot(power$Time, power$Global_reactive_power, type="l", col="black", xlab="datetime", ylab="Global_reactive_power")
+
 par(p)
 dev.off()
